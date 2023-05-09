556bce78275f2d23440bc01ac7b811713468ab8f
she110ff <youngsoo.j@gmail.com>
2023-05-09 13:22:23 +0900

    Add : authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
 import TodoPage from './pages/TodoPage';
+import SignIn from './pages/account/SignIn';
+import SignUp from './pages/account/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TodoPage />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="todo" element={<TodoPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/account/SignUp.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  Box,
+  Button,
+  Divider,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { Logo } from '../../components/layout/Logo';
+import { PasswordField } from '../../components/authentication/PasswordField';
+import OAuthButtonGroup from '../../components/authentication/OAuthButtonGroup';
+
+function SignUp() {
+  return (
+    <Stack spacing="8">
+      <Stack spacing="6">
+        <Logo />
+        <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
+          <Heading size={{ base: 'xs', md: 'sm' }}>Create an account</Heading>
+          <HStack spacing="1" justify="center">
+            <Text color="muted">Already have an accountt?</Text>
+            <Button variant="link" colorScheme="blue">
+              Log in
+            </Button>
+          </HStack>
+        </Stack>
+      </Stack>
+      <Box
+        py={{ base: '0', sm: '8' }}
+        px={{ base: '4', sm: '10' }}
+        bg={{ base: 'transparent', sm: 'bg-surface' }}
+        boxShadow={{ base: 'none', sm: 'md' }}
+        borderRadius={{ base: 'none', sm: 'xl' }}
+      >
+        <Stack spacing="6">
+          <Stack spacing="5">
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input id="email" type="email" />
+            </FormControl>
+            <PasswordField />
+          </Stack>
+          <Stack spacing="6">
+            <Button variant="primary">Sign Up</Button>
+            <HStack>
+              <Divider />
+              <Text fontSize="sm" whiteSpace="nowrap" color="muted">
+                or continue with
+              </Text>
+              <Divider />
+            </HStack>
+            <OAuthButtonGroup />
+          </Stack>
+        </Stack>
+      </Box>
+    </Stack>
+  );
+}
+
+export default SignUp;
